26e0e8764f27faca028f25bacd04040cf3b7bc57
Spencer Haney <theOfficialSkara@gmail.com>
2017-05-19 14:17:11 -0400

    Started core class

---
+++ src/main/java/com/rex/scarletvortex/ScarletVortex.java
@@ -0,0 +1,31 @@
+package com.rex.scarletvortex;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "ScarletVortex", name = "Scarlet Vortex", version = "1.10.2-0.1")
+public class ScarletVortex
+{
+    @Mod.Instance(value = "ScarletVortex")
+    public static ScarletVortex instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
